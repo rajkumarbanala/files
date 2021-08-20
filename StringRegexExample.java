10b65f92f37db47295a491fc7568c7e5ce62f357
Rajkumar Banala <36632302+rajkumarbanala@users.noreply.github.com>
2021-08-20 16:31:33 +0530

    String Regex Regular expression for date conversion

--- StringRegexExample.java
+++ StringRegexExample.java
@@ -1,16 +1,97 @@
-	public String convertAndPrepareDateFields(String value) {
+	public String fixAllDateTags(String value) {
 		
 		if(value == null)
 			return "";
 		
-		String regex = "....-..-..T..:..:..\\..........Z";
+		System.out.println("value:" + value);
 		
-		Matcher matcher = Pattern.compile(regex).matcher(value);
+		//" :2021-08-19T11: 18: 35.042006518Z
+		String regex1 = "\": ....-..-..T..:..:..\\..........Z";
+		
+		Matcher matcher = Pattern.compile(regex1).matcher(value);
 		
 		while (matcher.find()) {
+			
 			String matchValue = matcher.group();
-			String matchValueNew = "\"" + matchValue + "\"";
-			value = value.replace(matchValue, matchValueNew);
+			System.out.println("matchValue:" + matchValue);
+			
+			String regexSub = "....-..-..T..:..:..\\..........Z";
+			
+			Matcher matcherSub = Pattern.compile(regexSub).matcher(matchValue);
+			
+			while (matcherSub.find()) {
+				
+				String matchValueSub = matcherSub.group();
+				System.out.println("matchValueSub:" + matchValueSub);
+				
+				String matchValueSubNew = "\"" + matchValueSub;
+				System.out.println("matchValueSubNew:" + matchValueSubNew);
+				
+				String matchValueNew = "\": " + matchValueSubNew + "\"";
+				System.out.println("matchValueNew:" + matchValueNew);
+				
+				value = value.replace(matchValue, matchValueNew);
+			}
 		}
+		
+		//" :2021-08-19T16:39:13Z 
+		String regex2 = "\": ....-..-..T..:..:..Z";
+		
+		matcher = Pattern.compile(regex2).matcher(value);
+		
+		while (matcher.find()) {
+			
+			String matchValue = matcher.group();
+			System.out.println("matchValue:" + matchValue);
+			
+			String regexSub = "....-..-..T..:..:..Z";
+			
+			Matcher matcherSub = Pattern.compile(regexSub).matcher(matchValue);
+			
+			while (matcherSub.find()) {
+				
+				String matchValueSub = matcherSub.group();
+				System.out.println("matchValueSub:" + matchValueSub);
+				
+				String matchValueSubNew = "\"" + matchValueSub;
+				System.out.println("matchValueSubNew:" + matchValueSubNew);
+				
+				String matchValueNew = "\": " + matchValueSubNew + "\"";
+				System.out.println("matchValueNew:" + matchValueNew);
+				
+				value = value.replace(matchValue, matchValueNew);
+			}
+		}
+
+		//" :2021-08-19T16: 39: 13Z 
+		String regex3 = "\": ....-..-..T..: ..: ..Z";
+		matcher = Pattern.compile(regex3).matcher(value);
+		
+		while (matcher.find()) {
+			
+			String matchValue = matcher.group();
+			System.out.println("matchValue:" + matchValue);
+			
+			String regexSub = "....-..-..T..: ..: ..Z";
+			
+			Matcher matcherSub = Pattern.compile(regexSub).matcher(matchValue);
+			
+			while (matcherSub.find()) {
+				
+				String matchValueSub = matcherSub.group();
+				System.out.println("matchValueSub:" + matchValueSub);
+				
+				String matchValueSubNew = "\"" + matchValueSub;
+				System.out.println("matchValueSubNew:" + matchValueSubNew);
+				
+				String matchValueNew = "\": " + matchValueSubNew + "\"";
+				System.out.println("matchValueNew:" + matchValueNew);
+				
+				value = value.replace(matchValue, matchValueNew);
+			}
+		}
+		
+		System.out.println("value:" + value);
+		
 		return value;
 	}
